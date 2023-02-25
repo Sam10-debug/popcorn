f5849cadeba113355ae2817e93258de2258da64b
Emorinken <emorinkensam@gmail.com>
2023-02-25 04:35:39 -0800

    /

--- pages/comps/second.jsx
+++ pages/comps/second.jsx
@@ -12,18 +12,18 @@ const Second = () => {
         
         <p>You light up my world with your inner glow</p>
         <p>And I'll never let you go, that I know</p>
-        <p>With each passing day, you're more beautiful</p>
+        <p>With each passing day, youapos;re more beautiful</p>
         <p>My love for you will always be full.</p>
         
         <p>You bring so much joy to my life</p>
         <p>You are my heart, my soul, my wife</p>
         <p>Your laughter fills the air with delight</p>
-        <p>Together, we'll conquer all in sight.</p>
+        <p>Together, weapos;ll conquer all in sight.</p>
         
         <p>So here's a poem from my heart to you</p>
         <p>To show how much I truly love and appreciate you</p>
-        <p>Forever and always, I'll be by your side</p>
-        <p>Together we'll always enjoy the ride.</p>
+        <p>Forever and always, Iapos;ll be by your side</p>
+        <p>Together weapos;ll always enjoy the ride.</p>
         </div>
         <div class="btn-div">
 		<Link href="/comps/homep"><button>PREVIOUS</button></Link>
--- pages/comps/third.jsx
+++ pages/comps/third.jsx
@@ -5,7 +5,7 @@ const Third = () => {
 	<section className='third'>
 		<h1>POPCORN</h1>
 		<p>I admire your strength, your kindness, and your unwavering support. You make me a better person just by being in my life, and I am so thankful for everything you do for me. I never knew what true love was until I met you. Every day with you is a blessing and I am so excited to continue building a life together filled with love, laughter, and adventures.</p>
-		<p>I want you to know that I am here for you, always and forever. No matter what challenges we may face, I will always be by your side, holding your hand and supporting you every step of the way. I love you more than words could ever express and I can't wait to spend the rest of my life making you smile.</p>
+		<p>I want you to know that I am here for you, always and forever. No matter what challenges we may face, I will always be by your side, holding your hand and supporting you every step of the way. I love you more than words could ever express and I canapos;t wait to spend the rest of my life making you smile.</p>
 		<p>despite the fact that your head is dry and you dont really know anything like that,
 			yes despite that, 
 			love prevails,
