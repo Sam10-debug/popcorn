c90798c32a20298b6cb312266b38adf6a6b1b64e
Emorinken <emorinkensam@gmail.com>
2023-02-25 04:55:40 -0800

    /

--- pages/comps/homep.jsx
+++ pages/comps/homep.jsx
@@ -3,9 +3,9 @@ const Homep = () => {
   return (
 	<section className="text-layer" data-aos="fade-left">
 	<h1>Hey Babe,</h1>
-	<p>This was meant to be easy but  funny how the hardest part of all of this wasn&&apos;t writing the lines of codes to bring this to life it was actually finding the words that will do enough justice to describing how amazing you are and how much i love you</p>
+	<p>This was meant to be easy but  funny how the hardest part of all of this wasn&apos;t writing the lines of codes to bring this to life it was actually finding the words that will do enough justice to describing how amazing you are and how much i love you</p>
 	<p>Did I ever tell you that the hardest part of being with you is finding the perfect words to appreciate your entirety, time and time again, my lexicon fail me and no adjective is really doing enough to come close to qualifying you.</p>
-	<p>I randomly remember you are my woman and i grin from ear to ear because sometimes, it still feels surreal that you are mine and I don&&apos;t think I can ever say this enough, <h3>I LOVE YOU.</h3></p>  
+	<p>I randomly remember you are my woman and i grin from ear to ear because sometimes, it still feels surreal that you are mine and I don&apos;t think I can ever say this enough, <h3>I LOVE YOU.</h3></p>  
 	<div >
 		<Link href="/comps/second"><button>NEXT</button></Link>
 	</div>
--- pages/comps/second.jsx
+++ pages/comps/second.jsx
@@ -11,19 +11,19 @@ const Second = () => {
         <p>A true work of art, in every space.</p>
         
         <p>You light up my world with your inner glow</p>
-        <p>And I&&apos;ll never let you go, that I know</p>
-        <p>With each passing day, you&&apos;re more beautiful</p>
+        <p>And I&apos;ll never let you go, that I know</p>
+        <p>With each passing day, you&apos;re more beautiful</p>
         <p>My love for you will always be full.</p>
         
         <p>You bring so much joy to my life</p>
         <p>You are my heart, my soul, my wife</p>
         <p>Your laughter fills the air with delight</p>
-        <p>Together, we&&apos;ll conquer all in sight.</p>
+        <p>Together, we&apos;ll conquer all in sight.</p>
         
-        <p>So here&&apos;s a poem from my heart to you</p>
+        <p>So here&apos;s a poem from my heart to you</p>
         <p>To show how much I truly love and appreciate you</p>
-        <p>Forever and always, I&&apos;ll be by your side</p>
-        <p>Together we&&apos;ll always enjoy the ride.</p>
+        <p>Forever and always, I&apos;ll be by your side</p>
+        <p>Together we&apos;ll always enjoy the ride.</p>
         </div>
         <div class="btn-div">
 		<Link href="/comps/homep"><button>PREVIOUS</button></Link>
